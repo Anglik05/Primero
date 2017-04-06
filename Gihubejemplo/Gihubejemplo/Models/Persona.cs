1eeb8feb84a4fadbed6d00416e91d046c0febd2c
Anglik05 <ing.angelicarodriguez@gmail.com>
2017-04-06 17:46:09 +0200

    Nueva Clase
    Creacion de la clase persona

---
+++ Gihubejemplo/Gihubejemplo/Models/Persona.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Gihubejemplo.Models
+{
+    public class Persona
+    {
+        public int Edad { get; set; }
+        public string Nombre { get; set; }
+    }
+}
